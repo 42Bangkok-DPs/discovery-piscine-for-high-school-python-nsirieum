7cae95370bfa83420b22089fe53e4434eac4f858
nsirieum <tonpalmklang@gmail.com>
2024-10-10 05:46:09 +0700

    Add files via upload

---
+++ cell04/ex01/age.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+a = int(input("Please tell me your age : "))
+print("You are currrently " + str(a) + " years old.")
+
+add10years = a+10
+print("In 10 years, you'll be " + str(add10years) + " years old.")
+
+add20years = a+20
+print("In 20 years, you'll be " + str(add20years) + " years old.")
+
+add30years = a+30
+print("In 10 years, you'll be " + str(add30years) + " years old.")
