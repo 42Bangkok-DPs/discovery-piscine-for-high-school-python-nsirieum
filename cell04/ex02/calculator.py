fa2ed648efa243ad102e2b9ded01c8b0625f2c51
nsirieum <tonpalmklang@gmail.com>
2024-10-10 05:07:35 +0700

    Add files via upload

---
+++ cell04/ex02/calculator.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+a = int(input("Give me the first number: "))
+b = int(input("Give me the second number: "))
+print("Thank you!")
+add = a+b
+print(str(a) + ' + ' + str(b) + " = " ,add)
+minus = a-b
+print(str(a) + ' - ' + str(b) + " = " ,minus)
+divide = a/b
+print(str(a) + ' / ' + str(b) + " = " ,divide)
+multi = a*b
+print(str(a) + ' * ' + str(b) + " = " ,multi)
