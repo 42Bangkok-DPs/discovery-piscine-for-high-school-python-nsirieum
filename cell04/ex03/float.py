e53ebf2f572ffd6e30531fec8dc42dc0253920d5
nsirieum <tonpalmklang@gmail.com>
2024-10-10 10:49:21 +0700

    Delete cell04/ex03/float.py

--- cell04/ex03/float.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-
-a = input("Give me a number: ")
-if a == int(a) and a == float(a).split(" . ")[00]:
-    print("This number is an integer.")
-else:
-    print("This number is a decimal.")
-
